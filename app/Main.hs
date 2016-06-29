cf88078aecdd5b4490c3d25b197d3b1d29719551
Andreas Pauley <andreas@pauley.org.za>
2016-06-29 15:12:44 +0200

    import currentBranch from GitHellLib

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,13 @@ module Main where
 import Turtle
 import Prelude hiding (FilePath)
 import Data.Text (pack)
+import GitHellLib
 
 main :: IO ()
 main = do
   now <- date
   cwd <- pwd
+  stdout currentBranch
   let prompt = format (s%"\n"%fp%"$ ") (showText now) (basename cwd)
   echo prompt
 
