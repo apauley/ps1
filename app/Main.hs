43dfa13852b4f6d98d895fb60cc4963f9ba35d43
Andreas Pauley <andreas@pauley.org.za>
2016-07-09 22:15:32 +0200

    Add command-line options parser with --oneline support

--- app/Main.hs
+++ app/Main.hs
@@ -15,11 +15,19 @@ import qualified Control.Foldl as Fold
 
 main :: IO ()
 main = do
+  (oneline, trackBranch) <- options "Generates a git-aware shell prompt. export PS1='$(ps1)'" parser
+
   cwd      <- pwd
   timeLine <- getTimeLine
-  gitLine  <- getGitLine
-  let prompt = format (s%"\n"%s%fp%"$ ") timeLine gitLine (basename cwd)
+  gitLine  <- getGitLine $ not oneline
+  let prompt = if oneline
+        then format (s%" "%fp%"$ ") gitLine (basename cwd)
+        else format (s%"\n"%s%fp%"$ ") timeLine gitLine (basename cwd)
   echo prompt
+
+parser :: Parser (Bool, Maybe Text)
+parser = (,) <$> switch "oneline" '1' "Don't generate a multi-line prompt"
+             <*> optional (optText "track-merge-branch" 't' "Track if you're up to date with a branch that you may need to merge back to, eg. master")
 
 getTimeLine :: IO Text
 getTimeLine = do
@@ -29,16 +37,19 @@ getTimeLine = do
   let line = T.justifyRight (cols-1) '—' $ format (" "%s) time
   return $ darkGreyFG line
 
-getGitLine :: IO Text
-getGitLine = do
+getGitLine :: Bool -> IO Text
+getGitLine multiline = do
   shortStatus <- maybeFirstLine $ git "status" ["--short"]
   let modified = fromMaybe "" $ fmap (format ("\n"%s)) shortStatus
   let branchColour = if (shortStatus == Nothing) then greenFG else yellowFG
   branch <- colourOrEmpty branchColour currentBranch
   status <- colourOrEmpty upstreamColour gitStatusUpstream
+  let gitPrompt = if multiline
+        then format (s%" "%s%s%"\n") branch status modified
+        else format (s%" "%s) branch status
   let lines = if (T.null branch)
         then ""
-        else format (s%" "%s%s%"\n") branch status modified
+        else gitPrompt
   return lines
 
 upstreamColour :: Text -> Text
