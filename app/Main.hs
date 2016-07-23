80c303b1addf77856b43fd4d22a76fabe45bf741
Andreas Pauley <andreas@pauley.org.za>
2016-07-23 10:58:10 +0200

    Move functions to PromptLib

--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,7 @@ module Main where
 
 import Turtle
 import Prelude hiding (FilePath)
-import HSHLib (maybeFirstLine, terminalColumns)
-import GitHellLib (gitDiscardErr, currentBranchDiscardErr)
-import ANSIColourLib (cyanFG, darkGreyFG, greenFG, yellowFG, lightRedFG)
-import qualified Data.Text as T (justifyRight, null, pack, unpack, words)
-import Data.Maybe
-import qualified Data.Time.LocalTime as Time
-import qualified Data.Time.Format as TF
-import qualified Control.Foldl as Fold
+import PromptLib (getTimeLine, getGitLine)
 
 main :: IO ()
 main = do
@@ -28,41 +21,3 @@ main = do
 parser :: Parser (Bool, Maybe Text)
 parser = (,) <$> switch "oneline" '1' "Don't generate a multi-line prompt"
              <*> optional (optText "track-merge-branch" 't' "Track if you're up to date with a branch that you may need to merge back to, eg. master")
-
-getTimeLine :: IO Text
-getTimeLine = do
-  now  <- Time.getZonedTime
-  cols <- terminalColumns
-  let time = T.pack $ TF.formatTime TF.defaultTimeLocale "%Y-%m-%d %H:%M:%S" now
-  let line = T.justifyRight (cols-1) '—' $ format (" "%s) time
-  return $ darkGreyFG line
-
-getGitLine :: Bool -> IO Text
-getGitLine multiline = do
-  shortStatus <- maybeFirstLine $ gitDiscardErr "status" ["--short"]
-  let modified = fromMaybe "" $ fmap (format ("\n"%s)) shortStatus
-  let branchColour = if (shortStatus == Nothing) then greenFG else yellowFG
-  branch <- colourOrEmpty branchColour currentBranchDiscardErr
-  status <- colourOrEmpty upstreamColour gitStatusUpstream
-  let gitPrompt = if multiline
-        then format (s%" "%s%s%"\n") branch status modified
-        else format (s%" "%s) branch status
-  let lines = if (T.null branch)
-        then ""
-        else gitPrompt
-  return lines
-
-upstreamColour :: Text -> Text
-upstreamColour txt = if upToDate then cyanFG txt else lightRedFG txt
-  where upToDate = elem "up-to-date" $ T.words txt
-
-gitStatusUpstream :: Shell Text
-gitStatusUpstream = do
-  let searchText = "Your branch "
-  let st = sed (searchText *> return "") $ grep (prefix searchText) (gitDiscardErr "status" ["--long"])
-  sed ((choice [",", ".", "'"]) *> return "") st
-
-colourOrEmpty :: (Text -> Text) -> Shell Text -> IO Text
-colourOrEmpty colourFun shellText = do
-  line <- maybeFirstLine shellText
-  return $ fromMaybe "" $ fmap colourFun line
--- src/PromptLib.hs
+++ src/PromptLib.hs
@@ -1,2 +1,53 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module PromptLib where
 
+import Turtle
+import Prelude hiding (FilePath)
+
+import HSHLib (maybeFirstLine, terminalColumns)
+import GitHellLib (gitDiscardErr, currentBranchDiscardErr)
+import ANSIColourLib (cyanFG, darkGreyFG, greenFG, yellowFG, lightRedFG)
+import qualified Data.Text as T (justifyRight, null, pack, unpack, words)
+import Data.Maybe
+import qualified Data.Time.LocalTime as Time
+import qualified Data.Time.Format as TF
+import qualified Control.Foldl as Fold
+
+getTimeLine :: IO Text
+getTimeLine = do
+  now  <- Time.getZonedTime
+  cols <- terminalColumns
+  let time = T.pack $ TF.formatTime TF.defaultTimeLocale "%Y-%m-%d %H:%M:%S" now
+  let line = T.justifyRight (cols-1) '—' $ format (" "%s) time
+  return $ darkGreyFG line
+
+getGitLine :: Bool -> IO Text
+getGitLine multiline = do
+  shortStatus <- maybeFirstLine $ gitDiscardErr "status" ["--short"]
+  let modified = fromMaybe "" $ fmap (format ("\n"%s)) shortStatus
+  let branchColour = if (shortStatus == Nothing) then greenFG else yellowFG
+  branch <- colourOrEmpty branchColour currentBranchDiscardErr
+  status <- colourOrEmpty upstreamColour gitStatusUpstream
+  let gitPrompt = if multiline
+        then format (s%" "%s%s%"\n") branch status modified
+        else format (s%" "%s) branch status
+  let lines = if (T.null branch)
+        then ""
+        else gitPrompt
+  return lines
+
+upstreamColour :: Text -> Text
+upstreamColour txt = if upToDate then cyanFG txt else lightRedFG txt
+  where upToDate = elem "up-to-date" $ T.words txt
+
+gitStatusUpstream :: Shell Text
+gitStatusUpstream = do
+  let searchText = "Your branch "
+  let st = sed (searchText *> return "") $ grep (prefix searchText) (gitDiscardErr "status" ["--long"])
+  sed ((choice [",", ".", "'"]) *> return "") st
+
+colourOrEmpty :: (Text -> Text) -> Shell Text -> IO Text
+colourOrEmpty colourFun shellText = do
+  line <- maybeFirstLine shellText
+  return $ fromMaybe "" $ fmap colourFun line
