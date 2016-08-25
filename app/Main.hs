4b8430e100f05246c62ae8480e4e19c9c145ea00
Andreas Pauley <andreas@pauley.org.za>
2016-08-25 14:47:19 +0200

    Current dir

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,18 @@
 module Main where
 
 import Turtle
+import Prelude hiding (FilePath)
 import Data.Text (pack)
 
 main :: IO ()
 main = do
   now <- date
-  echo $ pack $ show now
+  cwd <- pwd
+  echo $ showText now
+  echo $ showFilePath cwd
+
+showText :: Show a => a -> Text
+showText s = pack $ show s
+
+showFilePath :: FilePath -> Text
+showFilePath path = (format fp) path
